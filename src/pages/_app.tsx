48840f202daffba8c51a72475b9798f4bf75903c
brestfamily <lemareccamille@gmail.com>
2025-10-05 20:35:07 +0200

    feat(favicon): utiliser /images/logo.png comme favicon et apple-touch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Inter, Playfair_Display } from 'next/font/google'
 import '@/styles/globals.css'
 import { CartProvider } from '@/contexts/CartContext'
@@ -9,6 +10,10 @@ const playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfa
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={`${inter.variable} ${playfair.variable}`}>
+      <Head>
+        <link rel="icon" href="/images/logo.png" type="image/png" />
+        <link rel="apple-touch-icon" href="/images/logo.png" />
+      </Head>
       <CartProvider>
         <Component {...pageProps} />
       </CartProvider>
