047a07d32823dd62f6980a8f0645abfbbf1f8fd8
brestfamily <lemareccamille@gmail.com>
2025-12-15 16:05:01 +0100

    fix: expose favicon 512 in document head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,24 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="robots" content="index, follow" />
           <meta property="og:site_name" content="Fanion Canon" />
+          <link
+            rel="icon"
+            href="/images/Favicon512.png"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            href="/images/Favicon512.png"
+            type="image/png"
+            sizes="192x192"
+          />
+          <link
+            rel="apple-touch-icon"
+            href="/images/Favicon512.png"
+            sizes="180x180"
+          />
+          <link rel="shortcut icon" href="/images/Favicon512.png" />
         </Head>
         <body>
           <Main />
